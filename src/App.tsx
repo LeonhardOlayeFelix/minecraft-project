3ac4475f16eb8c53d4df22e2442e74ec7a975309
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-24 20:54:39 +0100

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Grid></Grid>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"poster" "nav" "main" "footer"`,
+      }}
+    >
+      <Show above="lg">
+        <GridItem area={"poster"} bg={"green"}>
+          poster
+        </GridItem>
+      </Show>
+      <GridItem area={"nav"} bg={"coral"}>
+        nav
+      </GridItem>
+      <GridItem area={"main"} bg={"red"}>
+        main
+      </GridItem>
+      <GridItem area={"footer"} bg={"dodgerblue"}>
+        footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
