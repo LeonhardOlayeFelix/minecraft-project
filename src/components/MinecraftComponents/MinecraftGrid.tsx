51e75f59babb2921a8cc3e2572aec3a9ddf4999f
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-25 12:14:10 +0100

    Added masonry grid using react masonry grid library

--- src/components/MinecraftComponents/MinecraftGrid.tsx
+++ src/components/MinecraftComponents/MinecraftGrid.tsx
@@ -1,164 +1,32 @@
-import { Box, Flex, Show, SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Flex, Show, SimpleGrid } from "@chakra-ui/react";
 import useBlocksAndItems, { ItemsProps } from "../../hooks/useMinecraftHook";
 import MinecraftCardv2 from "./MinecraftCardv2";
+import Masonry from "react-masonry-css";
+import "./MinecraftGrid.css";
 interface Props {
   items: ItemsProps[];
   className?: string;
 }
 //width={{ xl: "1300px", lg: "1000px", md: "700px", sm: "400px" }}
 const MinecraftGrid = ({ items, className }: Props) => {
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
-    <div className={className}>
-      <Show above="1300px">
-        <Flex justifyContent={"space-between"} gap={4}>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 4 === 0 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 4 === 1 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 4 === 2 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 4 === 3 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-        </Flex>
-      </Show>
-      <Show above="1000px">
-        <Flex justifyContent={"space-between"} gap={4}>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 3 === 0 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 3 === 1 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 3 === 2 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-        </Flex>
-      </Show>
-      <Show above="700px">
-        <Flex justifyContent={"space-between"} gap={4}>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 2 === 0 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-          <SimpleGrid columns={1}>
-            {items.map(
-              (fff, index) =>
-                index % 2 === 1 && (
-                  <Box
-                    className="mb-3"
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <MinecraftCardv2 key={index} item={fff} />
-                  </Box>
-                )
-            )}
-          </SimpleGrid>
-        </Flex>
-      </Show>
-      <Show above="400px">
-        <Flex justifyContent={"space-between"} gap={4}>
-          <SimpleGrid columns={1}>
-            {items.map((fff, index) => (
-              <Box className="mb-3" display={"flex"} justifyContent={"center"}>
-                <MinecraftCardv2 key={index} item={fff} />
-              </Box>
-            ))}
-          </SimpleGrid>
-        </Flex>
-      </Show>
-    </div>
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {items.map((item) => (
+        <div>
+          <MinecraftCardv2 item={item} />
+        </div>
+      ))}
+    </Masonry>
   );
 };
 
