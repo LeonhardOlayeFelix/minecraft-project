46555c48e91f7aa1afeed1f3664a649e5d6e4e0a
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-26 23:59:20 +0100

    Adjusted parameters for color

--- src/components/MinecraftComponents/MinecraftItemCard.tsx
+++ src/components/MinecraftComponents/MinecraftItemCard.tsx
@@ -180,7 +180,7 @@ const MinecraftItemCard = ({ item, className, data }: Props) => {
       className={className}
       borderRadius="20px"
       bg={cardColor}
-      boxShadow={`0 0 10px 5px ${glowColor}`}
+      boxShadow={`0 0 15px 1px ${glowColor}`}
       overflow="hidden"
       transition="transform 0.2s"
       _hover={{ transform: "scale(1.02)" }}
