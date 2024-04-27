d8253167c8e99ae6f41277347b4554d9f09c6e0f
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-27 03:45:14 +0100

    Updated value of avatarhover color

--- src/components/MinecraftComponents/MinecraftItemCard.tsx
+++ src/components/MinecraftComponents/MinecraftItemCard.tsx
@@ -48,7 +48,7 @@ const MinecraftItemCard = ({ item, className, data }: Props) => {
   const [isGlowColorLoading, setIsGlowColorLoading] = useState(false);
   const [showRecipe, setShowRecipe] = useState(false);
   const [showUsedIn, setShowUsedIn] = useState(false);
-  const avatarHover = useColorModeValue("gray", "#202020");
+  const avatarHover = useColorModeValue("gray", "#20202050");
   const cardBodyBg = useColorModeValue("gray", "#202020");
   const textColor = useColorModeValue("gray.800", "white");
   const cardColor = useColorModeValue("white !important", "#111111");
@@ -205,83 +205,6 @@ const MinecraftItemCard = ({ item, className, data }: Props) => {
       w={{ base: "260px", md: "280px" }}
       minH="auto"
     >
-      {/* <Box h={"auto"} mb={1} pt="20px" pl="20px">
-        <Flex w="100%">
-          <Box
-            me="auto"
-            className="grow-1"
-            bg={cardBodybg}
-            borderRadius="12px"
-            padding={1}
-          >
-            {item.image && (
-              <Image
-                src={item.image}
-                transition={"transform 0.3s ease-in-out"}
-                _hover={{ transform: "scale(1.1)" }}
-              />
-            )}
-          </Box>
-          <Button
-            className="grow-1"
-            w="38px"
-            h="38px"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="12px"
-            me="13px"
-            bg={buttonColor}
-          >
-            <Icon
-              as={IoEllipsisHorizontalSharp}
-              color={iconColor}
-              w="24px"
-              h="24px"
-            />
-          </Button>
-        </Flex>
-        <Text
-          fontFamily="Roboto Remix"
-          fontWeight="500"
-          transition="color 0.2s"
-          color={textColor}
-          cursor={"pointer"}
-          _hover={{ color: textHoverColor }}
-          w="100%"
-          fontSize="35px"
-          onClick={() => console.log(item.name)}
-          lineHeight={"25px"}
-          mt={3}
-          paddingRight={3}
-        >
-          {item.name}
-        </Text>
-        <Flex justifyContent="left">
-          <AvatarGroup size="sm" max={4} fontSize="9px" fontWeight="700">
-            {matches.map((match, index) => (
-              <Tooltip
-                bg={tooltipBackground}
-                color={tooltipForeground}
-                hasArrow
-                label={match?.name}
-                key={index}
-              >
-                <Avatar
-                  className="grow-1 p-1"
-                  boxSize={8}
-                  onClick={() => console.log(match?.name)}
-                  cursor="pointer"
-                  border="white"
-                  src={match?.image}
-                  _hover={{
-                    bg: avatarHover,
-                  }}
-                />
-              </Tooltip>
-            ))}
-          </AvatarGroup>
-        </Flex>
-      </Box> */}
       <MinecraftItemCardHead
         bodyBg={cardBodyBg}
         buttonBg={buttonColor}
