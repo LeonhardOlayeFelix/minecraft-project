812a803171366e5553ac115659bb995473f066b8
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-05-11 19:15:21 +0100

    Set up query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./components/theme/theme.ts";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
