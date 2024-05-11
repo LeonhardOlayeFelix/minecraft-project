e41b633bf00750554d634ebab1011f760e2d522a
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-05-11 23:47:08 +0100

    Made a separate file for interface instances

---
+++ src/interfaces/Instances.ts
@@ -0,0 +1,22 @@
+export const beacon = {
+  item: "Beacon",
+  quantity: 1,
+  recipe: [
+    "Glass",
+    "Glass",
+    "Glass",
+    "Glass",
+    "Nether Star",
+    "Glass",
+    "Obsidian",
+    "Obsidian",
+    "Obsidian",
+  ],
+  shapeless: false,
+};
+export const noRecipe = {
+  item: "Empty",
+  quantity: 1,
+  recipe: ["Air", "Air", "Air", "Air", "Air", "Air", "Air", "Air", "Air"],
+  shapeless: false,
+};
